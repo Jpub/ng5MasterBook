ce80f16336ac2a23a5ab3e14d4a3a388c28ee8b7
Jpub <jeipubmanager@gmail.com>
2018-01-10 18:04:56 +0900

    initail commit

---
+++ ng-book-code/forms/src/app/demo-form-ng-model/demo-form-ng-model.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-demo-form-ng-model',
+  templateUrl: './demo-form-ng-model.component.html'
+})
+export class DemoFormNgModelComponent {
+  myForm: FormGroup;
+  productName: string;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'productName':  ['', Validators.required]
+    });
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
+  }
+}
---
+++ ng-book-code/routes/music/src/environments/spotifyApiKey.ts
@@ -0,0 +1,2 @@
+// this file should be auto-generated when you run `npm start`
+export const SpotifyAPIKey = 'Run npm start to generate this -- SEE DOCS';
